c3134dc764e2ae6c520167022be9dd25aa63ecba
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 13:39:26 +0000

    Mon Apr 08 13:39:26 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,7 +1,7 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
-  GOODS = {"A" => 50, "B" => 30, "C" => 20, "D"=> 15}
-  DEALS = {"AAA" => -20, "BB" => -15}
+  GOODS = {"A" => 50, "B" => 30, "C" => 20, "D"=> 15, "E"=> 40}
+  DEALS = {"AAA" => -20, "BB" => -15, "AAAAA" => -50, "EEB" => -30}
   def checkout(basket)
     value = 0
     GOODS.each_pair do |key, price|
@@ -10,8 +10,12 @@ class Checkout
         value += price if item == key
       end
     end
+  if basket.count("A") > 3 && basket.count("A") < 5
+
+  end
     value += DEALS["AAA"] * (basket.count("A")/3)
     value += DEALS["BB"] * (basket.count("B")/2)
   end
 
 end
+
