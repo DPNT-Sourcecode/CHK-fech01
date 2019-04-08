77f0a8f016ea377aaf039ac600b029fbd38d5098
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 13:14:15 +0000

    Mon Apr 08 13:14:15 UTC 2019

--- lib/solutions/CHK/spec/checkout_spec.rb
+++ lib/solutions/CHK/spec/checkout_spec.rb
@@ -3,7 +3,7 @@ require "./checkout"
 describe Checkout do
 
   shop = Checkout.new
-
+context "Invalid basket" do
   it "Returns -1 for an invalid input a" do
     expect(shop.checkout("a")).to eq -1
   end
@@ -11,6 +11,24 @@ describe Checkout do
   it "Returns -1 for an invalid input F" do
     expect(shop.checkout("F")).to eq -1
   end
+end
+
+context "Single Items" do
+  it "Returns an 50 for A" do
+    expect(shop.checkout("A")).to eq 50
+  end
+  it "Returns an 30 for B" do
+    expect(shop.checkout("B")).to eq 30
+  end
+  it "Returns an 20 for C" do
+    expect(shop.checkout("C")).to eq 20
+  end
+  it "Returns an 15 for D" do
+    expect(shop.checkout("D")).to eq 15
+  end
+end
+
 
 end
+
 
