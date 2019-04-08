8c105ef4e071074347f543db2420e819f7ee7337
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 13:20:15 +0000

    Mon Apr 08 13:20:15 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -2,12 +2,15 @@
 class Checkout
   GOODS = {"A" => 50, "B" => 30, "C" => 20, "D"=> 15}
   def checkout(basket)
+    value = 0
     GOODS.each_pair do |key, value|
-      return value if basket == key
+      basket.each do |item|
+      end
     end
     -1
   end
 
 end
+
 
 
--- lib/solutions/CHK/spec/checkout_spec.rb
+++ lib/solutions/CHK/spec/checkout_spec.rb
@@ -3,32 +3,55 @@ require "./checkout"
 describe Checkout do
 
   shop = Checkout.new
-context "Invalid basket" do
-  it "Returns -1 for an invalid input a" do
-    expect(shop.checkout("a")).to eq -1
+  context "Invalid basket" do
+    it "Returns -1 for an invalid input a" do
+      expect(shop.checkout("a")).to eq -1
+    end
+
+    it "Returns -1 for an invalid input F" do
+      expect(shop.checkout("F")).to eq -1
+    end
   end
 
-  it "Returns -1 for an invalid input F" do
-    expect(shop.checkout("F")).to eq -1
+  context "Single Items" do
+    it "Returns an 50 for A" do
+      expect(shop.checkout("A")).to eq 50
+    end
+    it "Returns an 30 for B" do
+      expect(shop.checkout("B")).to eq 30
+    end
+    it "Returns an 20 for C" do
+      expect(shop.checkout("C")).to eq 20
+    end
+    it "Returns an 15 for D" do
+      expect(shop.checkout("D")).to eq 15
+    end
+  end
+
+  context "Multiple different items" do
+    it "Returns an 100 for AA" do
+      expect(shop.checkout("AA")).to eq 100
+    end
+    it "Returns an 100 for AB" do
+      expect(shop.checkout("AB")).to eq 80
+    end
+    it "Returns an 100 for AC" do
+      expect(shop.checkout("AC")).to eq 70
+    end
+    it "Returns an 100 for AD" do
+      expect(shop.checkout("AD")).to eq 65
+    end
+    it "Returns an 100 for BA" do
+      expect(shop.checkout("BA")).to eq 80
+    end
+    it "Returns an 100 for AA" do
+      expect(shop.checkout("BC")).to eq 50
+    end
+    it "Returns an 100 for AA" do
+      expect(shop.checkout("BD")).to eq 45
+    end
   end
 end
 
-context "Single Items" do
-  it "Returns an 50 for A" do
-    expect(shop.checkout("A")).to eq 50
-  end
-  it "Returns an 30 for B" do
-    expect(shop.checkout("B")).to eq 30
-  end
-  it "Returns an 20 for C" do
-    expect(shop.checkout("C")).to eq 20
-  end
-  it "Returns an 15 for D" do
-    expect(shop.checkout("D")).to eq 15
-  end
-end
-
-
-end
 
 
