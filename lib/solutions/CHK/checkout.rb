75507b0614272c4dbdfe5255a64a5e3280c5acd6
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 13:17:15 +0000

    Mon Apr 08 13:17:15 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,9 +1,13 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
-
+  GOODS = {"A" => 50, "B" => 30, "C" => 20, "D"=> 15}
   def checkout(basket)
+    GOODS.each_pair do |key, value|
+      return value if basket == key
+    end
     -1
   end
 
 end
+
 
